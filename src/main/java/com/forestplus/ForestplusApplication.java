2cf59e1ad4b4c699f584813145fbf05989692a9b
Juan Medina <soyjuanmedina@gmail.com>
2025-10-20 12:35:54 +0200

    Generate war

--- src/main/java/com/forestplus/ForestplusApplication.java
+++ src/main/java/com/forestplus/ForestplusApplication.java
@@ -2,10 +2,18 @@ package com.forestplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = "com.forestplus")
-public class ForestplusApplication {
+@SpringBootApplication
+public class ForestplusApplication extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(ForestplusApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ForestplusApplication.class);
     }
 }
