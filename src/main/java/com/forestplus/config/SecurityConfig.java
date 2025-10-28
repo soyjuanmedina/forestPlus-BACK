294775552e0b1356b913d8b0d6b01becfdaf85b5
Juan Medina <soyjuanmedina@gmail.com>
2025-10-28 19:03:53 +0100

    setAllowedOrigins

--- src/main/java/com/forestplus/config/SecurityConfig.java
+++ src/main/java/com/forestplus/config/SecurityConfig.java
@@ -71,7 +71,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of(
+        	    "http://localhost:4200",
+        	    "https://forestplusapp.com",
+        	    "https://www.forestplusapp.com"
+        	));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
