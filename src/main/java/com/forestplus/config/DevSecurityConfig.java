5bc1f48644b02b076f4d024968611eaef38ee403
Juan Medina <soyjuanmedina@gmail.com>
2026-02-01 18:32:48 +0100

    ventana de login navegador

--- src/main/java/com/forestplus/config/DevSecurityConfig.java
+++ src/main/java/com/forestplus/config/DevSecurityConfig.java
@@ -1,28 +1,21 @@
 package com.forestplus.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@Profile("dev")    // 👈 SOLO development
+@Profile("dev")    // SOLO development
 @EnableMethodSecurity
-@RequiredArgsConstructor
 public class DevSecurityConfig {
-
-    private final JwtAuthenticationFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -32,18 +25,17 @@ public class DevSecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().authenticated() // TODO el WAR dev protegido
             )
-            .httpBasic(); // 👈 usa la ventana de login del navegador, muy simple
+            .httpBasic(); // 👈 ventana de login navegador
 
         return http.build();
     }
 
-    // 👤 Usuario DEV (candado)
+    // Usuario DEV (candado)
     @Bean
-    public UserDetailsService devUserDetailsService(PasswordEncoder encoder) {
-
+    public UserDetailsService devUserDetailsService() {
         return new InMemoryUserDetailsManager(
-                User.withUsername("dev")    // usuario fijo
-                .password("{noop}clave") // contraseña sin encriptar
+            User.withUsername("dev")    // usuario fijo
+                .password("{noop}clave") // contraseña literal
                 .roles("DEV")
                 .build()
         );
